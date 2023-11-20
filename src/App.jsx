cc6277aaeb4c554cac5e7bd81233e7a5461e064b
shivProgramer <shivapal6390@gmail.com>
2023-11-20 17:25:08 +0530

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,16 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
+import {fetchDataFromApi} from './utils/api' 
 function App() {
-  const [count, setCount] = useState(0)
+  useEffect(()=>{
+    apiTesting();
+  },[])
+
+  const apiTesting = ()=>{
+    fetchDataFromApi('/movie/popular').then((res)=>{
+       console.log("res",res);
+    });
+  };
+  
   return (
     <>
     <div className="App"> App</div>
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,23 +1,26 @@
 import axios from "axios";
+import { VITE_APP_TMDB_TOKEN } from "../../config";
 
 const BASE_URL = "https://api.themoviedb.org/3";
 
-const TMDB_TOKEN = import.meta.env.VITE_APP_TMDB_TOKEN; 
+const TMDB_TOKEN = VITE_APP_TMDB_TOKEN
 
 const headers = {
   Authorization: "bearer " + TMDB_TOKEN,
 };
 
 export const fetchDataFromApi = async (url, params)=>{
-
     try {
+
+        console.log("Token",TMDB_TOKEN);
         const { data } = await axios.get(BASE_URL + url, {
          headers,
          params
         })
         return data;
     } catch (error) {
-        console.log(error);
+        console.log(error); 
         return error;
     }
 }
+ 
